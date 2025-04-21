613b3d66a253923704785d15282eb9171d9a20a6
Juan Manuel Franco Osorio <juan.franco26551@ucaldas.edu.co>
2025-04-21 11:19:08 -0500

    Commit con Jetstream

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,24 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return /*view('welcome');*/[
-        "mensaje" => "Bienvenido a mi app"
-    ];
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/saludo', function () {
-    return [
-        "esatdo" => true,
-        "mensaje" => "Hola, soy un saludo",
-        "codigo" => 200
-    ];
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 });
